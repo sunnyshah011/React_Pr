a1f32572db567aa2a8ed17632e8ba9bd71b11454
sunnyshah011 <sunnyshah35350@gmail.com>
2025-07-24 20:49:34 +0545

    side nav deleted and new added

---
+++ Side_Nav/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidenav from './Sidenav';
+
+function App() {
+  return (
+    <div className='h-[200vh] bg-amber-100 '>
+      <Sidenav />
+    </div>
+  );
+}
+
+export default App;
